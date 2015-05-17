c86513d596bc8051f176f3fc3ad56a0a72a5bd03
lanbing510 <xiaosonglan@gmail.com>
2015-05-18 00:46:05 +0800

    Create DouBanSpider.py
    爬下豆瓣刚兴趣标签下的所有图书，按评分存储到Excel，Version 1.0

---
+++ DouBanSpider.py
@@ -0,0 +1,93 @@
+import sys
+import time
+import urllib
+import requests
+from bs4 import BeautifulSoup
+from openpyxl import Workbook
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def book_spider(book_tag):
+    page_num=0;
+    count=1
+    book_list=[]
+    try_times=0
+    
+    while(page_num<3):
+        url="http://www.douban.com/tag/"+urllib.quote(book_tag)+"/book?start="+str(page_num*15)
+        source_code = requests.get(url)
+        time.sleep(0.3)
+        
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        list_soup = soup.find('div', {'class': 'mod book-list'})
+        
+        try_times+=1;
+        if list_soup==None and try_times<200:
+            continue
+        elif list_soup==None:
+            break # Break when no informatoin got after 200 times requesting
+        
+        for book_info in list_soup.findAll('dd'):
+            title = book_info.find('a', {'class':'title'}).string.strip()
+            desc = book_info.find('div', {'class':'desc'}).string.strip()
+            desc_list = desc.split('/')
+            
+            try:
+                author_info = '作者/译者： ' + '/'.join(desc_list[0:-3])
+            except:
+                author_info='作者/译者： 暂无'
+            try:
+                pub_info = '出版信息： ' + '/'.join(desc_list[-3:])
+            except:
+                pub_info='出版信息： 暂无'
+            try:
+                rating = book_info.find('span', {'class':'rating_nums'}).string.strip()
+            except:
+                rating='0.0'
+            try:
+                people_num = book_info.findAll('span')[2].string.strip()
+                people_num=people_num.strip('人评价')
+            except:
+                people_num='0'
+            
+            book_list.append([title,rating,people_num,author_info,pub_info])
+            try_times=0 #set 0 when got valid information
+        page_num+=1
+        print "Downloading Information From Page %d" % page_num
+    return book_list
+
+
+def do_spider(book_tag_lists):
+    book_lists=[]
+    for book_tag in book_tag_lists:
+        book_list=book_spider(book_tag)
+        book_list=sorted(book_list,key=lambda x:x[1],reverse=True)
+        book_lists.append(book_list)
+    return book_lists
+
+
+def print_book_lists_excel(book_lists,book_tag_lists):
+    wb=Workbook(optimized_write=True)
+    ws=[]
+    for i in range(len(book_tag_lists)):
+        ws.append(wb.create_sheet(title=book_tag_lists[i].decode())) #utf8->unicode
+    for i in range(len(book_tag_lists)): 
+        ws[i].append(['序号','书名','评分','评价人数','作者','出版社'])
+        count=1
+        for bl in book_lists[i]:
+            ws[i].append([count,bl[0],bl[1],bl[2],bl[3],bl[4]])
+            count+=1
+    wb.save('book_list.xlsx')
+
+
+
+if __name__=='__main__':
+    book_tag_lists = ['认知心理学','历史','经济','心理']
+    
+    book_lists=do_spider(book_tag_lists)
+    print_book_lists(book_lists,book_tag_lists)
+    print_book_lists_excel(book_lists,book_tag_lists)
+    
